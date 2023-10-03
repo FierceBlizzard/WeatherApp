204f750862304b4b7a7305903a3262d6be1f77cb
Iclyn Taero <43586417+FierceBlizzard@users.noreply.github.com>
2023-10-02 22:13:43 -0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by IceMac on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
